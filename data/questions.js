e3b49aa136a05d86514fd74ad9e63002dd17ede7
Nguyen Tan Tri <nguyentantri.a4@gmail.com>
2023-12-21 01:39:22 +0700

    quiz-project

---
+++ data/questions.js
@@ -0,0 +1,160 @@
+
+export default [
+  {
+      question: "Việt Nam thuộc lục địa nào?",
+      options:[
+          {
+              id:"0",
+              options:"A",
+              answer:"Nam Mỹ",
+          },
+          {
+              id:"1",
+              options:"B",
+              answer:"Châu Âu",
+          },
+          {
+              id:"2",
+              options:"C",
+              answer:"Châu Á",
+          },
+          {
+              id:"0",
+              options:"D",
+              answer:"Ấn Độ",
+          },
+      ],
+      correctAnswerIndex: 2
+  },
+  {
+      question: "Châu lục nào có mật độ dân số cao nhất? ",
+      options:[
+          {
+              id:"0",
+              options:"A",
+              answer:"Châu Á",
+          },
+          {
+              id:"1",
+              options:"B",
+              answer:"Nam Phi",
+          },
+          {
+              id:"2",
+              options:"C",
+              answer:"Châu Úc",
+          },
+          {
+              id:"0",
+              options:"D",
+              answer:"Nam Cực",
+          },
+      ],
+      correctAnswerIndex: 0,
+
+  },
+  {
+      question: "5 x 5 = ?",
+      options:[
+          {
+              id:"0",
+              options:"A",
+              answer:"20",
+          },
+          {
+              id:"1",
+              options:"B",
+              answer:"25",
+          },
+          {
+              id:"2",
+              options:"C",
+              answer:"10",
+          },
+          {
+              id:"0",
+              options:"D",
+              answer:"30",
+          },
+      ],
+      correctAnswerIndex: 1
+  },
+  {
+      question: "Căn bậc hai của 169 là bao nhiêu",
+      options:[
+          {
+              id:"0",
+              options:"A",
+              answer:"20",
+          },
+          {
+              id:"1",
+              options:"B",
+              answer:"23",
+          },
+          {
+              id:"2",
+              options:"C",
+              answer:"13",
+          },
+          {
+              id:"0",
+              options:"D",
+              answer:"23",
+          },
+      ],
+      correctAnswerIndex: 2
+  },
+  {
+      question: "Đại dương nhỏ nhất là gì?",
+      options:[
+          {
+              id:"0",
+              options:"A",
+              answer:"Đại Tây Dương",
+          },
+          {
+              id:"1",
+              options:"B",
+              answer:"Thái Bình Dương",
+          },
+          {
+              id:"2",
+              options:"C",
+              answer:"Bắc Băng Dương",
+          },
+          {
+              id:"0",
+              options:"D",
+              answer:"Ấn Độ Dương",
+          },
+      ],
+      correctAnswerIndex: 2
+  },
+  {
+    question: "Dòng họ nào nhiều nhất tại Việt Nam?",
+    options:[
+        {
+            id:"0",
+            options:"A",
+            answer:"Nguyễn",
+        },
+        {
+            id:"1",
+            options:"B",
+            answer:"Trần",
+        },
+        {
+            id:"2",
+            options:"C",
+            answer:"Lý",
+        },
+        {
+            id:"0",
+            options:"D",
+            answer:"Đào",
+        },
+    ],
+    correctAnswerIndex: 0
+}
+]
